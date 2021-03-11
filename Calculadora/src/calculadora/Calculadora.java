2928e4a651ed28d741338736a12cba4fbbdce086
DAM1 <DAM1@DESKTOP-OUBA795>
2021-03-11 12:25:04 +0100

    Agregado el método potencia

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -21,4 +21,9 @@ public class Calculadora {
     
     public static double divide(double num1,double num2){
         return num1/num2;
+    }
+    
+    public static double potencia(double num1,double num2){
+        return Math.pow(num1, num2);
+    }
 }
