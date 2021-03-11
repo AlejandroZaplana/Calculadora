4305bc1000962ad79f8c3a352a8651f7757fa86a
DAM1 <DAM1@DESKTOP-OUBA795>
2021-03-11 11:49:25 +0100

    Agregado el método multiplicar

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author DAM1
+ */
+public class Calculadora {
+
+    public static double sumar(double num1, double num2){
+        return num1+num2;
+    }
+ 
+    public static double multiplicar(double num1,double num2){
+        return num1*num2;
+    }
+}
